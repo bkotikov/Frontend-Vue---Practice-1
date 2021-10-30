b1ee3f7f64fea5bc68e593411b99c8af5dfe3425
bkotikov <46450521+bkotikov@users.noreply.github.com>
2021-10-30 15:09:43 +0200

    Frontend Vue - Practice 1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,495 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="header-container">
+      <Header />
+      <div class="header-desk">
+        <div class="container-fluid mt-3 h-100 d-flex">
+          <div class="row h-100 align-items-center">
+            <div class="col-6 my-auto align-self-center">
+              <h2 class="header-desk-header">
+                Want anything to be easy with LaslesVPN.
+              </h2>
+              <p class="header-desk-body">
+                Provide a network for all your needs with ease and fun using
+                LaslesVPN discover interesting features from us.
+              </p>
+
+              <button class="header-desk-btn">Get Started</button>
+            </div>
+            <div class="col-6 my-auto align-self-center justify-content-center">
+              <img
+                class="img-fluid"
+                src="../src/assets/Illustration.svg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="info-section py-3 px-2 row mt-5 mx-auto text-center">
+      <div class="col-4 d-flex justify-content-center">
+        <div class="icon-user">
+          <img src="../src/assets/user.svg" alt="" srcset="" />
+        </div>
+        <div class="user-information ms-3">
+          <span class="user-span">90+</span> <br />
+          Users
+        </div>
+      </div>
+      <div class="col-4 d-flex justify-content-center">
+        <div class="icon-user">
+          <img src="../src/assets/location.svg" alt="" srcset="" />
+        </div>
+        <div class="location-information ms-3">
+          <span class="location-span">30+</span><br />
+          Location
+        </div>
+      </div>
+      <div class="col-4 d-flex">
+        <div class="icon-user">
+          <img src="../src/assets/Server.svg" alt="" srcset="" />
+        </div>
+        <div class="server-information ms-3">
+          <span class="server-span">50+</span> <br />
+          Server
+        </div>
+      </div>
+    </div>
+
+    <div class="features-section mt-5 row d-flex align-items-center">
+      <div class="features-img col-6">
+        <img class="img-fluid" src="../src/assets/Illustration_2.svg" alt="" />
+      </div>
+      <div class="features-text col-6">
+        <h3 class="features-txt-header">
+          We Provide Many <br />
+          Features You Can Use
+        </h3>
+        <p class="features-txt-body">
+          You can explore the features that we provide with fun and have their
+          own functions each feature.
+        </p>
+        <ul class="features-list">
+          <li class="features-txt-list">Powerfull online protection.</li>
+          <li class="features-txt-list">Internet without borders.</li>
+          <li class="features-txt-list">Supercharged VPN</li>
+          <li class="features-txt-list">No specific time limits.</li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="plan-section mt-5">
+      <div class="plan-section-txt">
+        <h2 class="plan-section-txt-header">Choose Your Plan</h2>
+
+        <p class="plan-section-txt-body">
+          Let's choose the package that is best for you and explore it happily
+          and cheerfully.
+        </p>
+      </div>
+
+      <div class="plan-section-cards d-flex mt-5 justify-content-between">
+        <div class="plan-section-card">
+          <img src="../src/assets/Free.svg" alt="" />
+          <h3 class="card-header mt-3">Free Plan</h3>
+          <ul class="card-lists mt-3">
+            <li class="card-list">Unlimited Bandwidth</li>
+            <li class="card-list">Encrypted Connection</li>
+            <li class="card-list">No Traffic Logs</li>
+            <li class="card-list">Works on All Devices</li>
+          </ul>
+
+          <h4 class="card-title mt-5">Free</h4>
+
+          <button class="card-btn mt-2">Select</button>
+        </div>
+        <div class="plan-section-card">
+          <img src="../src/assets/Free.svg" alt="" />
+          <h3 class="card-header mt-3">Free Plan</h3>
+          <ul class="card-lists mt-3">
+            <li class="card-list">Unlimited Bandwidth</li>
+            <li class="card-list">Encrypted Connection</li>
+            <li class="card-list">No Traffic Logs</li>
+            <li class="card-list">Works on All Devices</li>
+          </ul>
+
+          <h4 class="card-title mt-5">Free</h4>
+
+          <button class="card-btn mt-2">Select</button>
+        </div>
+        <div class="plan-section-card">
+          <img src="../src/assets/Free.svg" alt="" />
+          <h3 class="card-header mt-3">Free Plan</h3>
+          <ul class="card-lists mt-3">
+            <li class="card-list">Unlimited Bandwidth</li>
+            <li class="card-list">Encrypted Connection</li>
+            <li class="card-list">No Traffic Logs</li>
+            <li class="card-list">Works on All Devices</li>
+          </ul>
+
+          <h4 class="card-title mt-5">Free</h4>
+
+          <button class="card-btn mt-2">Select</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="global-section mt-5">
+      <div class="global-section-txt">
+        <h2 class="global-section-txt-header">
+          Huge Global Network <br />
+          of Fast VPN
+        </h2>
+
+        <p class="global-section-txt-body">
+          See LaslesVPN everywhere to make it easier for you when you move
+          locations.
+        </p>
+      </div>
+
+      <div class="global-section-img mt-5 my-auto">
+        <img
+          class="img-fluid w-75"
+          src="../src/assets/Huge_Global.svg"
+          alt=""
+        />
+      </div>
+      <div
+        class="
+          global-section-sponsored
+          d-flex
+          mt-3
+          justify-content-between
+          align-items-center
+        "
+      >
+        <img
+          class="img-fluid w-50 h-25"
+          src="../src/assets/Mask_Group.svg"
+          alt=""
+        />
+        <img
+          class="img-fluid w-50 h-25"
+          src="../src/assets/Mask_Group-1.svg"
+          alt=""
+        />
+        <img
+          class="img-fluid w-50 h-25"
+          src="../src/assets/Mask_Group-2.svg"
+          alt=""
+        />
+        <img
+          class="img-fluid w-50 h-25"
+          src="../src/assets/Mask_Group-3.svg"
+          alt=""
+        />
+      </div>
+    </div>
+
+    <div class="recomendation-section mt-3">
+      <div class="global-section-txt">
+        <h2 class="recomendation-section-txt-header">
+          Trusted by Thousands of <br />
+          Happy Customer
+        </h2>
+
+        <p class="recomendation-section-txt-body">
+          These are the stories of our customers who have joined us with great
+          pleasure when using this crazy feature.
+        </p>
+      </div>
+
+      <div class="recomendation-cards mt-3 d-flex horizontal-scroll-wrapper">
+        
+        <div class="recomendation-card d-flex align-items-center justify-content-between mt-3 flex-column">
+          <div class="d-flex align-items-center justify-content-between">
+            <div class="recomendation-card-header d-flex align-items-center justify-content-between">
+              <img src="../src/assets/profile.svg" alt="" />
+              <p class="recomendation-card-header-body">Viezh Robert <br />Warsaw, Poland</p>
+            </div>
+
+            <div class="recomendation-card-header-rating ms-4">
+              4.5
+              <img src="../src/assets/Vector.svg" alt="" />
+            </div>
+          </div>
+
+          <div class="user-message  w-75">
+            <p>
+              “Wow... I am very happy to use this VPN, it turned out to be more
+              than my expectations and so far there have been no problems.
+              LaslesVPN always the best”.
+            </p>
+          </div>
+        </div>
+
+        <div class="recomendation-card d-flex align-items-center justify-content-between mt-3 flex-column">
+          <div class="d-flex align-items-center justify-content-between align-items-center">
+            <div class="recomendation-card-header d-flex align-items-center justify-content-between">
+              <img src="../src/assets/profile.svg" alt="" />
+              <p class="recomendation-card-header-body">Viezh Robert <br />Warsaw, Poland</p>
+            </div>
+
+            <div class="recomendation-card-header-rating ms-4">
+              4.5
+              <img src="../src/assets/Vector.svg" alt="" />
+            </div>
+          </div>
+
+          <div class="user-message  w-75">
+            <p>
+              “Wow... I am very happy to use this VPN, it turned out to be more
+              than my expectations and so far there have been no problems.
+              LaslesVPN always the best”.
+            </p>
+          </div>
+        </div>
+        
+        <div class="recomendation-card d-flex align-items-center justify-content-between mt-3 flex-column">
+          <div class="d-flex align-items-center justify-content-between">
+            <div class="recomendation-card-header d-flex align-items-center justify-content-between">
+              <img src="../src/assets/profile.svg" alt="" />
+              <p class="recomendation-card-header-body">Viezh Robert <br />Warsaw, Poland</p>
+            </div>
+
+            <div class="recomendation-card-header-rating ms-4">
+              4.5
+              <img src="../src/assets/Vector.svg" alt="" />
+            </div>
+          </div>
+
+          <div class="user-message  w-75">
+            <p>
+              “Wow... I am very happy to use this VPN, it turned out to be more
+              than my expectations and so far there have been no problems.
+              LaslesVPN always the best”.
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "../src/components/Header.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+  },
+};
 </script>
 
 <style>
+body,
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  background-color: #fcfcfc;
+}
+
+.header-desk-header {
+  font-style: normal;
+  font-weight: 500;
+  font-size: 3.125rem;
+  line-height: 4.375rem;
+  padding: 0.5rem 0;
+  margin: 0;
+}
+
+.header-desk-body {
+  font-style: normal;
+  font-weight: 400;
+  font-size: 0.9rem;
+  line-height: 1.5rem;
+  color: #4f5665;
+}
+
+.header-desk-btn {
+  width: 180px;
+  height: 40px;
+  font-size: 1rem;
+  font-style: bold;
+  line-height: 1.5rem;
+  color: #fff;
+  background-color: #f53838;
+  border: #f53838 solid 1px;
+  border-radius: 0.75rem;
+  position: relative;
+
+  -webkit-box-shadow: 0px 13px 20px -5px rgba(245, 56, 56, 0.59);
+  box-shadow: 0px 13px 20px -5px rgba(245, 56, 56, 0.59);
+}
+
+.info-section {
+  background-color: #fff;
+  -webkit-box-shadow: 0px 50px 50px -6px rgba(0, 0, 0, 0.13);
+  box-shadow: 0px 50px 50px -6px rgba(0, 0, 0, 0.13);
+}
+
+.user-information,
+.location-information,
+.server-information {
+  font-size: 1.25rem;
+  color: #000;
+  font-weight: 500;
+  font-style: normal;
+}
+
+.user-span,
+.location-span,
+.server-span {
+  font-weight: 700 !important;
+}
+
+.features-txt-header,
+.global-section-txt-header,
+.plan-section-txt-header,
+.recomendation-section-txt-header {
+  font-style: normal;
+  font-weight: 500;
+  font-size: 2.5rem;
+  line-height: 50px;
+}
+
+.features-txt-body,
+.global-section-txt-body,
+.plan-section-txt-body,
+.recomendation-section-txt-body {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 1rem;
+  line-height: 30px;
+  color: #4f5665;
+  font-weight: 400;
+}
+
+.features-list {
+  position: relative;
+  list-style: none;
+  margin-left: 0;
+  padding-left: 2rem;
+}
+
+.features-txt-list::before {
+  content: "\2713";
+  background-color: rgb(0, 172, 29);
+  padding: 0.01rem 0.25rem;
+  color: #fff;
+  border-radius: 100%;
+  position: absolute;
+  left: 0;
+}
+
+.features-txt-list:nth-child(n + 2) {
+  margin-top: 1rem;
+}
+
+.plan-section {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.card-header {
+  font-style: normal;
+  font-weight: 500;
+  font-size: 1.25rem;
+  line-height: 30px;
+  background-color: #fff !important;
+}
+
+.plan-section-card {
+  padding: 2rem 3rem;
+  background: #fff;
+  border: #dfdfdf solid 2px;
+  border-radius: 1rem;
+}
+
+.card-lists {
+  position: relative;
+  list-style: none;
+  margin-left: 0;
+  padding-left: 1.5rem;
+}
+
+.card-list::before {
+  content: "\2713";
+  color: rgb(0, 199, 33);
+  border-radius: 100%;
+  position: absolute;
+  left: 0;
+}
+
+.card-title {
+  font-style: normal;
+  font-weight: 500;
+  font-size: 1.5rem;
+  line-height: 2rem;
+}
+
+.card-btn {
+  padding: 0.5rem 2.5rem;
+  border: #f53838 solid 1px;
+  background-color: #fff;
+  color: #f53838;
+  border-radius: 1rem;
+}
+
+.card-btn:hover {
+  color: #fff;
+  background-color: #f53838;
+  -webkit-box-shadow: 0px 37px 36px 8px rgba(245, 56, 56, 0.25);
+  box-shadow: 0px 37px 36px 8px rgba(245, 56, 56, 0.25);
+}
+.global-section,
+.recomendation-section {
+  text-align: center;
+}
+
+.user-message {
+  display: block;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 0.75rem;
+  line-height: 1.5rem;
+  text-align: left !important;
+}
+
+.horizontal-scroll-wrapper{
+  flex-wrap: no-wrap;
+  overflow-x: auto;
+}
+
+.horizontal-scroll-wrapper > div{
+  flex: 0 0 auto;
+  width: 400px;
+  height: 100%;
+  max-width: 100%;
+}
+
+.recomendation-card{
+  background-color: #fff;
+  padding: 2rem 1rem;
+  border-width: 2px !important;
+  border-color: #dfdfdf !important;
+  border-style: solid;
+  border-radius: 0.75rem;
+}
+
+.recomendation-card:nth-child(n + 2){
+  margin: 0 0 0 1rem;
+}
+
+.recomendation-card:focus, .recomendation-card:hover{
+  border-color: #f53838 !important;
+}
+
+.recomendation-card-header-body{
+  margin: 0 0 0 1rem;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="header-container d-flex justify-content-between align-items-center">
+        <div class="header-logo">
+            <img class="img-fluid" src="../assets/Logo-2.svg" alt="">
+        </div>
+        <div class="header-menu">
+            <a href="#" class="header-link-btn link-btn-normalize">About</a>
+            <a href="#" class="header-link-btn link-btn-normalize">Features</a>
+            <a href="#" class="header-link-btn link-btn-normalize">Pricing</a>
+            <a href="#" class="header-link-btn link-btn-normalize">Testimonials</a>
+            <a href="#" class="header-link-btn link-btn-normalize">Help</a>
+        </div>
+        <div class="header-sign">
+            <a href="#" class="header-link-sign-btn link-btn-normalize">Sign In</a>
+            <a href="#" class="header-link-sign-btn active-header-link link-btn-normalize">Sign up</a>
+        </div>
+    </div>
+</template>
+
+
+<script>
+
+export default{
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+.header-container{
+    width: 100%;
+    height: 80px;
+}
+
+.header-logo, .header-menu, .header-sign{
+    display: inline-block;
+    padding: 0;
+    margin: 0;
+}
+
+.link-btn-normalize{
+    text-decoration: none;
+    color: rgb(146, 146, 146);
+}
+
+.header-menu .header-link-btn:nth-child(n + 2), .header-sign .header-link-sign-btn:nth-child(n + 2){
+    margin-left: 0.5rem;
+}
+
+.header-link-btn, .header-link-sign-btn{
+    font-style: normal;
+    font-size: 0.9rem;
+    font-weight: 400;
+    padding: 0.25rem;
+}
+
+.header-link-btn, .header-link-sign-btn{
+    font-style: normal;
+    font-size: 0.9rem;
+    font-weight: 400;
+    padding: 0.25rem;
+}
+
+.header-sign .header-link-sign-btn:last-child{
+    padding: 0.5rem 1.25rem !important;
+}
+
+.active-header-link{
+    border: 1px solid #F53855;
+    box-sizing: border-box;
+    border-radius: 60px;
+}
+
+</style>
